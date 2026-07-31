ffa0c89e8851ce7932cf62ed0938f03ca7c0a624
taigong26 <thetaigon@qq.com>
2026-07-31 18:08:53 +0800

    fix(param)

--- launch/ros2/chassis_impedance.launch.py
+++ launch/ros2/chassis_impedance.launch.py
@@ -7,6 +7,8 @@
 ################################################################
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
@@ -16,6 +18,13 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     impedance_pkg_path = FindPackageShare('hex_ros_demo_chassis_impedance')
     urdf_pkg_path = FindPackageShare('hex_ros_urdf_maver_x4')
+
+    # args
+    use_sim_time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        choices=['true', 'false'],
+        description='Flag to use simulation time (/clock)')
 
     # chassis_impedance node
     impedance_param_path = PathJoinSubstitution(
@@ -33,7 +42,7 @@ def generate_launch_description():
             impedance_param_path,
             {
                 "model_urdf": ParameterValue(urdf_file_path, value_type=str),
-                "use_sim_time": True,
+                "use_sim_time": LaunchConfiguration('use_sim_time'),
             },
         ],
         remappings=[
@@ -44,5 +53,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        use_sim_time_arg,
         chassis_impedance_node,
     ])
